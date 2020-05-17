d29b5be7365315d73e85a2436fabf4e9e6c3f430
katsumi-hamaguchi <raikaraika7722@gmail.com>
2020-05-17 20:16:30 +0900

    登録画面実装中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+
+
+protected
+  def after_sign_in_path_for(resource) #サインインした時の移動先の指定（本来は商品一覧画面に行く予定だが、現状はトップページに指定。修正が必要）
+    root_path(resource)
+  end
+
+  
+  def after_sign_out_path_for(resource) #サインアウト後の移動先の指定（トップページ指定）
+    root_path(resource)
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :postcode, :address, :phone_number, :password, :password_confirmation])
+    #sign_upの際にnameのデータ操作を許。追加したカラム。
+  end
 end
+
--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,16 @@
 class CustomersController < ApplicationController
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update		
+	end
+
+	def update_retire		
+	end
+
+	
 end
