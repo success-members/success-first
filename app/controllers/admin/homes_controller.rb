9b9c278d6f826820f4419c1a1205c1c74da4f922
vagrant <vagrant@localhost.localdomain>
2020-05-18 05:29:11 +0000

    管理者ログイン画面作成

---
+++ app/controllers/admin/homes_controller.rb
@@ -0,0 +1,4 @@
+class Admin::HomesController < ApplicationController
+	def home
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+  	devise_for :admins
+  root 'admins#top' #最初の画面をルートにて設定しています。
+  end
 end
