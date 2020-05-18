c06c30157dc01fc322b318976963c699cc55df0b
vagrant <vagrant@localhost.localdomain>
2020-05-18 06:23:29 +0000

    controllerにアクション追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers
 
-  namespace :admins do
+  namespace :admin do
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
