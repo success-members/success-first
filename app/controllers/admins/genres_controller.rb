2312309a78c3da03af919b97ee80c1f6cc272cda
vagrant <vagrant@localhost.localdomain>
2020-05-18 02:59:56 +0000

    ジャンル登録・一覧・編集画面 作成中

--- app/controllers/admins/genres_controller.rb
+++ app/controllers/admins/genres_controller.rb
@@ -1,2 +1,33 @@
 class Admins::GenresController < ApplicationController
+
+	# ジャンル登録・一覧画面
+	def new
+		@genres = Genre.all
+		@genre = Genre.new
+	end
+
+	# ジャンルの新規登録をする
+	def create
+		@genre = Genre.new(genre_params)
+		@genre.save
+		redirect_to new_admins_genre_path
+	end
+
+	# ジャンル編集画面
+	def edit
+		@genre = Genre.find(1)
+	end
+
+	# ジャンルの更新をする
+	def update
+		@genre = Genre.find(1)
+		@genre.update(genre_params)
+		redirect_to new_admins_genre_path
+	end
+
+
+	private
+	def genre_params
+		params.permit(:name, :is_valid)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :customers
 
   namespace :admins do
+  	resources :genres, only: [:new, :create, :edit, :update]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
