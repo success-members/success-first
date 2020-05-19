dfd858ddaee444252d99fbcced89012371831adb
mrBuild19 <rabits.tank@gmail.com>
2020-05-19 09:36:57 +0000

    Add radio-bottun

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,16 @@
 class OrdersController < ApplicationController
 	def new
 		@shipping_address = ShippingAddress.where(customer_id: current_customer.id)
+		@order = Order.new
+	end
+
+	def confirm
+		@order = params.require(:order)
+		# @order = ShippingAddress.new(order_params)
+	end
+
+	private
+	def order_params
+		params.require(:order).permit(:postcode, :address, :name)
 	end
 end
