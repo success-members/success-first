709d38e5aa1cbb3f3eeda9a241a7f937e480c4c4
vagrant <vagrant@localhost.localdomain>
2020-05-19 10:59:57 +0000

    [Update] view

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -2,5 +2,6 @@ class Admin::HomesController < ApplicationController
 
 	# トップページ画面
 	def top
+		# @orders = Orders.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
 	end
 end
