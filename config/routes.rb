6d1aeb42e3856064f966b775fd472ed66d652563
vagrant <vagrant@localhost.localdomain>
2020-05-19 13:05:14 +0000

    1st commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :admins
   devise_for :customers
 
+  namespace :admin do
+  	resources :customers, only: [:index, :show, :edit, :update]
+  end
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
