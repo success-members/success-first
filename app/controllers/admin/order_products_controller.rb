f0114b3fc14b7e2ac0616a5d3c055c5e6a96c019
vagrant <vagrant@localhost.localdomain>
2020-05-20 08:48:04 +0000

    オーダー関連作成

--- app/controllers/admin/order_products_controller.rb
+++ app/controllers/admin/order_products_controller.rb
@@ -1,2 +1,10 @@
 class Admin::OrderProductsController < ApplicationController
+	def index
+		@products = Product.all
+		
+	end
+	private
+	def product_params
+		params.require(:product).permit(:name, :is_sale)
+	end
 end
