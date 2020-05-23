1439ea5db2f29ba910be3f74ff22d77dea2e6c1c
mrBuild19 <rabits.tank@gmail.com>
2020-05-23 14:17:08 +0000

    Fix トップ画面におすすめ商品を表示（ランダムで）

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,6 @@
 class HomesController < ApplicationController
 	def top
+		random_products = Product.where(is_sale: true).order(Arel.sql("RANDOM()"))
+		@recommend_products = random_products.select { |p| p.genre.is_valid }
 	end
 end
