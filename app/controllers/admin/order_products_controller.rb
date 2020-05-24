a998ab5b5f210bda46a1bc898ab2d3a7572320bb
mrBuild19 <rabits.tank@gmail.com>
2020-05-24 22:41:27 +0000

    Fix 制作ステータスが一つでも「製作中」になると注文ステータスが「製作中」に自動更新する

--- app/controllers/admin/order_products_controller.rb
+++ app/controllers/admin/order_products_controller.rb
@@ -7,6 +7,9 @@ class Admin::OrderProductsController < ApplicationController
 	def update
 		@order_product = OrderProduct.find(params[:id])
 		@order_product.update(order_product_params)
+		if @order_product.making_status_before_type_cast == 2
+			@order_product.order.update(order_status: 2)
+		end
 		redirect_to admin_order_path(@order_product.order)
 	end
 
