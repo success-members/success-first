2f3225bf4708017948cfc7c1f7777bcd95b5d448
katsumi-hamaguchi <raikaraika7722@gmail.com>
2020-05-25 12:24:32 +0900

    ステータス関連

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -11,9 +11,24 @@ class Admin::OrdersController < ApplicationController
 		@total_amount = @order.billing - @order.postage
 	end
 
+	#def update
+		#@order = Order.find(params[:id])
+		  #@order.update(order_params)
+		#redirect_to admin_order_path(@order)
+	#end
+
 	def update
 		@order = Order.find(params[:id])
-		  @order.update(order_params)
+		  if @order.update(order_params)
+		  	if @order.order_status_before_type_cast == 1
+				@order.order_products.update(making_status: 1)
+				redirect_to admin_order_path(@order)
+			else
+				redirect_to admin_order_path(@order)
+			end
+		else
+			redirect_to admin_order_path(@order)
+		end
 	end
 
 	private
