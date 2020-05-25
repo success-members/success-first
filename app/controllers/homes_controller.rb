d185312b1fce67c441f48302c78628946bf2d596
mrBuild19 <rabits.tank@gmail.com>
2020-05-25 10:32:52 +0000

    Fix トップ画面のおすすめ商品に税率を適応させる

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,6 @@
 class HomesController < ApplicationController
 	def top
+		@tax = 1.1 # 税率10%
 		@genres = Genre.where(is_valid: true)
 		random_products = Product.where(is_sale: true).order(Arel.sql("RANDOM()"))
 		@recommend_products = random_products.select { |p| p.genre.is_valid }
