0b87f18130b377f64c2c93a0b72f9f15bff40dd0
mrBuild19 <rabits.tank@gmail.com>
2020-05-29 05:05:11 +0000

    Fix RuntimeError

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'b6174a2aac11acf0b99eeea904bc6cca58dcee81b8acb3439f52686270d16feb6e3bbe23e4d0c5bd0ecdbb010832873ebe9e648bb01f94ee61c34d80564c9abc'
