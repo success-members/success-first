e0f6342994e01702f992ff2fee582817e3417097
mrBuild19 <rabits.tank@gmail.com>
2020-05-29 05:23:16 +0000

    mysql用に差し替え

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -2,7 +2,7 @@ class HomesController < ApplicationController
 	def top
 		@tax = 1.1 # 税率10%
 		@genres = Genre.where(is_valid: true)
-		random_products = Product.where(is_sale: true).order(Arel.sql("RANDOM()"))
+		random_products = Product.where(is_sale: true).order(Arel.sql("RAND()"))
 		@recommend_products = random_products.last(4).select { |p| p.genre.is_valid }
 	end
 end
--- db/migrate/20200517074016_add_genre_id_to_products.rb
+++ db/migrate/20200517074016_add_genre_id_to_products.rb
@@ -4,7 +4,7 @@ class AddGenreIdToProducts < ActiveRecord::Migration[5.2]
     add_column :products, :name, :string, null: false, default: ""
     add_column :products, :image, :string, null: false, default: ""
     add_column :products, :price, :integer, null: false, default: 0
-    add_column :products, :explanation, :text, null: false, default: ""
+    add_column :products, :explanation, :text, null: false
     add_column :products, :is_sale, :boolean, null: false, default: true
     
   end
